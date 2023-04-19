c8a0ee2d1b245e73a6f97f81e1c7b9f7dbe379a1
helen-tan <helentanmz0909@gmail.com>
2023-04-19 16:45:06 +0800

    Using types

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -17,32 +17,52 @@ import { Component } from "@angular/core";
 
 export class UserComponent {
     // Properties
-    firstName = 'John';
-    lastName = 'Smith';
-    age = 30;
-    address = {
-        street: '50 Main st',
-        city: 'Boston',
-        state: 'MA'
-    }
+    firstName: string;
+    lastName: string;
+    age: number;
+    address;
+
+    foo: any;
+    hasKids: boolean;
+    numberArray: number[];
+    stringArray: string[];
+    mixedArray: any[];
+    myTuple: [string, number, boolean];
+    unusable: void;
+    u: undefined;
+    n: null;
 
     // Methods
 
     // constructor runs when component is initialized
     // constructor is usually used to inject dependencies
     constructor() { 
-       
-    }
+       this.firstName = 'John';
+       this.lastName = 'Doe';
+       this.age = 30;
+       this.address = {
+        street: '50 Main st',
+        city: 'Boston',
+        state: 'MA'
+       }
+       this.foo = true;
+       this.hasKids = true;
+       this.numberArray = [1, 2, 3];
+       this.stringArray = ['hello', 'world']; 
+       this.mixedArray = [1, true, undefined, 'hey'];
+       this.myTuple = ['hello', 1, true];
+       this.unusable = undefined;
+       this.u = undefined;
+       this.n = null;
 
-    sayHello() {
-        console.log(`Hello ${this.firstName}`);
-    }
-
-    hasBirthday() {
-        this.age += 1;
+       console.log(this.addNumbers(2, 3));
     }
 
     showAge() {
         return this.age + 2;
+    }
+
+    addNumbers(num1: number, num2: number): number {
+        return num1 + num2;
     }
 }
