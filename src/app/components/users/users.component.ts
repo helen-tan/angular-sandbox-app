c24ca94bc20591da5f639aba85acf3f8d2c929a7
helen-tan <helentanmz0909@gmail.com>
2023-04-20 16:41:52 +0800

    Conditionals with *ngIf - mimicking server req with setTimeout

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -9,43 +9,51 @@ import { User } from 'src/app/models/User';
 })
 
 export class UsersComponent implements OnInit {
-  users!: User[];
+  users: User[] = [];
   showExtended: boolean = true;
+  loaded: boolean = false;
+
+  constructor() {
+    // Simulating a request to fetch users
+    setTimeout(() => {
+      this.users = [
+        {
+          firstName: 'John',
+          lastName: 'Doe',
+          age: 30,
+          address: {
+            street: '50 Main st',
+            city: 'Boston',
+            state: 'MA'
+          }
+        },
+        {
+          firstName: 'Kevin',
+          lastName: 'Johnson',
+          age: 34,
+          address: {
+            street: '20 School st',
+            city: 'Lynn',
+            state: 'MA'
+          }
+        },
+        {
+          firstName: 'Karen',
+          lastName: 'Williams',
+          age: 26,
+          address: {
+              street: '55 Mill st',
+              city: 'Miami',
+              state: 'FL'
+          }
+        }
+      ];
+      // Changed the loaded property to true
+      this.loaded = true
+    }, 2000);
+  }
 
   ngOnInit() {
-    this.users = [
-      // {
-      //   firstName: 'John',
-      //   lastName: 'Doe',
-      //   age: 30,
-      //   address: {
-      //     street: '50 Main st',
-      //     city: 'Boston',
-      //     state: 'MA'
-      //   }
-      // },
-      // {
-      //   firstName: 'Kevin',
-      //   lastName: 'Johnson',
-      //   age: 34,
-      //   address: {
-      //     street: '20 School st',
-      //     city: 'Lynn',
-      //     state: 'MA'
-      //   }
-      // },
-      // {
-      //   firstName: 'Karen',
-      //   lastName: 'Williams',
-      //   age: 26,
-      //   address: {
-      //       street: '55 Mill st',
-      //       city: 'Miami',
-      //       state: 'FL'
-      //   }
-      // }
-    ];
-
     this.showExtended = false;
 
     this.addUser({
