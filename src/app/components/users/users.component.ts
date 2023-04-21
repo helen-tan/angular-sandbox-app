ccdcc92e9526611d7f68773da1fb19d31456eeb9
helen-tan <helentanmz0909@gmail.com>
2023-04-21 16:23:28 +0800

    Code cleanup

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -13,8 +13,6 @@ export class UsersComponent implements OnInit {
   showExtended: boolean = true;
   loaded: boolean = false;
   enableAdd: boolean = true;
-  currentClasses = {};
-  currentStyles = {};
 
   constructor() {}
 
@@ -29,9 +27,7 @@ export class UsersComponent implements OnInit {
           city: 'Boston',
           state: 'MA'
         },
-        image: "https://unsplash.it/640?image=91",
         isActive: true,
-        balance: 100,
         registered: new Date('01/02/2023 08:30:00')
       },
       {
@@ -43,9 +39,7 @@ export class UsersComponent implements OnInit {
           city: 'Lynn',
           state: 'MA'
         },
-        image: "https://unsplash.it/640?image=65",
         isActive: false,
-        balance: 200,
         registered: new Date('3/11/2023 06:20:00')
       },
       {
@@ -57,9 +51,7 @@ export class UsersComponent implements OnInit {
             city: 'Miami',
             state: 'FL'
         },
-        image: "https://unsplash.it/600?image=64",
         isActive: true,
-        balance: 50,
         registered: new Date('11/02/2023 10:30:00')
       }
     ];
@@ -67,33 +59,9 @@ export class UsersComponent implements OnInit {
     // Changed the loaded property to true
     this.loaded = true
 
-
-    // this.addUser({
-    //   firstName: 'David',
-    //   lastName: 'Jackson',
-    // })
-
-    this.setCurrentClasses();
-    this.setCurrentStyles();
   }
 
   addUser(user: User) {
     this.users.push(user);
-  }
-
-  // For class binding with ngClass
-  setCurrentClasses() {
-    this.currentClasses = {
-      'btn-success': this.enableAdd, // The class 'btn-success' will be applied if the property enableAdd is true
-      'big-text': this.showExtended
-    }
-  }
-
-  // For style binding with ngStyle
-  setCurrentStyles() {
-    this.currentStyles = {
-      'padding-top': this.showExtended ? '0' : '40px',
-      'font-size': this.showExtended ? '' : '40px'
-    }
   }
 }
