df6b405c2f861ae43f547fdcab5972436bf24e0f
helen-tan <helentanmz0909@gmail.com>
2023-04-21 10:45:43 +0800

    Conditional rendering with ngIf

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -13,58 +13,49 @@ export class UsersComponent implements OnInit {
   showExtended: boolean = true;
   loaded: boolean = false;
 
-  constructor() {
-    // Simulating a request to fetch users
-    setTimeout(() => {
-      this.users = [
-        {
-          firstName: 'John',
-          lastName: 'Doe',
-          age: 30,
-          address: {
-            street: '50 Main st',
-            city: 'Boston',
-            state: 'MA'
-          }
-        },
-        {
-          firstName: 'Kevin',
-          lastName: 'Johnson',
-          age: 34,
-          address: {
-            street: '20 School st',
-            city: 'Lynn',
-            state: 'MA'
-          }
-        },
-        {
-          firstName: 'Karen',
-          lastName: 'Williams',
-          age: 26,
-          address: {
-              street: '55 Mill st',
-              city: 'Miami',
-              state: 'FL'
-          }
-        }
-      ];
-      // Changed the loaded property to true
-      this.loaded = true
-    }, 2000);
-  }
+  constructor() {}
 
   ngOnInit() {
-    this.showExtended = false;
+    this.users = [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 30,
+        address: {
+          street: '50 Main st',
+          city: 'Boston',
+          state: 'MA'
+        }
+      },
+      {
+        firstName: 'Kevin',
+        lastName: 'Johnson',
+        age: 34,
+        address: {
+          street: '20 School st',
+          city: 'Lynn',
+          state: 'MA'
+        }
+      },
+      {
+        firstName: 'Karen',
+        lastName: 'Williams',
+        age: 26,
+        address: {
+            street: '55 Mill st',
+            city: 'Miami',
+            state: 'FL'
+        }
+      }
+    ];
+
+    // Changed the loaded property to true
+    this.loaded = true
+
 
     this.addUser({
       firstName: 'David',
       lastName: 'Jackson',
-      age: 44,
-      address: {
-          street: '12 Wake st',
-          city: 'Miami',
-          state: 'FL'
-      }
     })
   }
 
