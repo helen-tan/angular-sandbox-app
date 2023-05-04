882e7ee2fdcf2a3c9fd83076076585e85c038097
helen-tan <helentanmz0909@gmail.com>
2023-05-04 15:17:19 +0800

    Created post-form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { PostsComponent } from './components/posts/posts.component';
 
 import { UserService } from './services/user.service';
 import { PostService } from './services/post.service';
+import { PostFormComponent } from './components/post-form/post-form.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { PostService } from './services/post.service';
     UserComponent,
     UsersComponent,
     NavbarComponent,
-    PostsComponent
+    PostsComponent,
+    PostFormComponent
   ],
   imports: [
     BrowserModule,
