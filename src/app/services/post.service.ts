62e963939dcdf6c1aa092d0b1e3d6938dfa9f560
helen-tan <helentanmz0909@gmail.com>
2023-05-05 11:15:47 +0800

    Delete post

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -33,4 +33,10 @@ export class PostService {
 
     return this.http.put<Post>(url, post, httpOptions);
   }
+  removePost(post: Post | number): Observable<Post> {
+    const id = typeof post === 'number' ? post : post.id;
+    const url = `${this.postUrl}/${id}`;
+
+    return this.http.delete<Post>(url, httpOptions);
+  }
 }
